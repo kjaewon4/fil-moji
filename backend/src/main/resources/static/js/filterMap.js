0d3494a94e717f0c652633cf7378c39b23479d99
kjaewon4 <jaewon0002@gmail.com>
2025-05-23 03:35:25 +0900

    이모지 매핑 완료

--- backend/src/main/resources/static/js/filterMap.js
+++ backend/src/main/resources/static/js/filterMap.js
@@ -15,29 +15,13 @@ const FILTER_MAP = {
       width: 200,
       height: 200
     },
-    emoji1: {
-        src: "",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
-    },
     emoji2: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/2.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
-    },
-    emoji3: {
-        src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/3.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+         landmarkIndex: 27, // 미간
+         offsetX: 0,
+         offsetY: -100,
+         width: 200,
+         height: 200
     },
     emoji4: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/4.png",
@@ -49,130 +33,106 @@ const FILTER_MAP = {
     },
     emoji5: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/5.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+         landmarkIndex: 27, // 미간
+         offsetX: 0,
+         offsetY: -100,
+         width: 250,
+         height: 250
     },
     emoji6: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/6.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+         landmarkIndex: 27, // 미간
+         offsetX: 0,
+         offsetY: -80,
+         width: 200,
+         height: 200
     },
     emoji7: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/7.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+         landmarkIndex: 27, // 미간
+         offsetX: 0,
+         offsetY: -80,
+         width: 200,
+         height: 200
     },
     emoji8: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/8.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+         landmarkIndex: 27, // 미간
+         offsetX: 0,
+         offsetY: 10,
+         width: 250,
+         height: 250
     },
-    emoji9: {
+    emojiPigNose: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/9.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+         landmarkIndex: 30, // 미간
+         offsetX: 0,
+         offsetY: 0,
+         width: 50,
+         height: 50
     },
     emoji10: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/10.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 66,
+        offsetX: 0,
+        offsetY: 0,
+        width: 60,
+        height: 60
     },
     emoji11: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/11.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 66,
+        offsetX: 0,
+        offsetY: 0,
+        width: 60,
+        height: 60
     },
     emoji12: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/12.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
-    },
-    emoji13: {
-        src: "",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
-    },
-    emoji14: {
-        src: "",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 66,
+        offsetX: 0,
+        offsetY: 0,
+        width: 60,
+        height: 60
     },
     emoji15: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/15.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 27,
+        offsetX: 0,
+        offsetY: 5,
+        width: 170,
+        height: 170
     },
     emoji16: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/16.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 27,
+        offsetX: 0,
+        offsetY: 30,
+        width: 150,
+        height: 150
     },
     emoji17: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/17.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 27,
+        offsetX: 0,
+        offsetY: 0,
+        width: 200,
+        height: 200
     },
     emoji18: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/18.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 46,  // 오른쪽 동공
+        offsetX: 0,
+        offsetY: 20,
+        width: 100,
+        height: 100
     },
     emoji19: {
         src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/19.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
-    },
-    emoji20: {
-        src: "https://raw.githubusercontent.com/che1206/deeplearning/2b275373bb7dbcf1ea9be9a8f7e8771011f9a857/deep/emo/20.png",
-        landmarkIndex: 10,
-        offsetX: -25,
-        offsetY: -80,
-        width: 50,
-        height: 50
+        landmarkIndex: 42,
+        offsetX: 0,
+        offsetY: 10,
+        width: 200,
+        height: 200
     }
 };
